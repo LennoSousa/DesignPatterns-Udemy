d98a6d4d6deb8305340b1a6f6b3871c8fce9287f
Lenno Sousa <lenno_sousa@hotmail.com>
2023-01-18 15:17:12 -0300

    Aplicando o padrão de projeto ChainOfResponsibility.

---
+++ DesignPatterns-Udemy/DP-ChainOfResponsibility/Middlewares/CheckPermissionMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_ChainOfResponsibility.Middlewares
+{
+    internal class CheckPermissionMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (email.Equals("master@hcode.com.br"))
+            {
+                Console.WriteLine("Seja bem-vindo Administrador.");
+                return true;
+            }
+
+            Console.WriteLine("Seja bem-vindo!");
+
+            return CheckNext(email, password);
+        }
+    }
+}
---
+++ DesignPatterns-Udemy/DP-ChainOfResponsibility/Middlewares/CheckUserMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using DP_ChainOfResponsibility.Servers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_ChainOfResponsibility.Middlewares
+{
+    internal class CheckUserMiddleware : Middleware
+    {
+        private Server server;
+
+        public CheckUserMiddleware(Server server)
+        {
+            this.server = server;
+        }
+
+        public override bool Check(string email, string password)
+        {
+            if (!server.HasEmail(email))
+            {
+                Console.WriteLine("E-mail inválido!");
+                return false;
+            }
+
+            if (!server.IsValidPassword(email, password))
+            {
+                Console.WriteLine("E-mail e/ou Senha inválidos!");
+                return false;
+            }
+
+            return CheckNext(email, password);
+        }
+    }
+}
