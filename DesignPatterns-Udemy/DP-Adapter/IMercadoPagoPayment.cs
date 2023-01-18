893ecf80898d713477ba63f98a1106cfb5439730
Lenno Sousa <lenno_sousa@hotmail.com>
2023-01-17 21:58:36 -0300

    Aplicando o padrão de projeto Adapter

---
+++ DesignPatterns-Udemy/DP-Adapter/IMercadoPagoPayment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_Adapter
+{
+    internal interface IMercadoPagoPayment
+    {
+        Token AuthToken();
+        void EnviaPagamento();
+        void RecebePaamento();
+    }
+}
---
+++ DesignPatterns-Udemy/DP-Adapter/PayoneerAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_Adapter
+{
+    internal class PayoneerAdapter : IPayPalPayment
+    {
+        private Payoneer payoneer;
+
+        public PayoneerAdapter(Payoneer payoneer)
+        {
+            this.payoneer = payoneer;
+            Console.WriteLine("Realizando adaptação de Payoneer para os métodos do PayPal.");
+        }
+
+        public Token AuthToken()
+        {
+            return this.payoneer.AuthToken();
+        }
+
+        public void PayPalPayment()
+        {
+            this.payoneer.SendPayment();
+        }
+
+        public void PayPalReceive()
+        {
+            this.payoneer.ReceivePayment();
+        }
+    }
+}
